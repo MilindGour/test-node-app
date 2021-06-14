8ff43a06c32e9aaf9d94638fc91043e0ab9bf619
Milind Gour <gour.milind@gmail.com>
2021-06-14 22:15:11 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/hello", (req, res) => {
+  res.status(200).send("Hello World from node js server!!");
+});
+
+const server = app.listen(7749, () => {
+  const address = server.address();
+  console.log("listening on port", address.port);
+});
